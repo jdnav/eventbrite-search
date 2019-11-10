615cca6653e6abbbfb4af55d5f5f0958aa442664
jdnav <josedaniel.navarro@edu.uah.es>
2019-11-10 14:39:13 +0100

    Form component ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from "./components/Header";
 import CategoryProvider from "./context/CategoriesContext";
+import Form from "./components/Form";
 
 function App() {
 
   return (
     <CategoryProvider>
       <Header title="Search for events with Eventbrite API"/>
+      <div className="uk-container">
+        <Form />
+      </div>
     </CategoryProvider>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  state = {
+    name: "",
+    category: ""
+  };
+  render() {
+    return (
+      <form>
+        <fieldset className="uk-fieldset uk-margin">
+          <legend className="uk-legend uk-text-center">
+            Search by name or category
+          </legend>
+        </fieldset>
+
+        <div className="uk-column-1-3@m uk-margin">
+          {/** Input for name */}
+          <div className="uk-margin" uk-margin="true">
+            <input
+              name="name"
+              className="uk-input"
+              type="text"
+              placeholder="Name of the event or city"
+            />
+          </div>
+          {/** Select for category */}
+          <div className="uk-margin" uk-margin="true">
+            <select className="uk-select" name="category">
+              {/** Options come from backend */}
+            </select>
+          </div>
+          {/** Submit button */}
+          <div className="uk-margin" uk-margin="true">
+            <input
+              className="uk-button uk-button-danger"
+              type="submit"
+              value="Search events"
+            />
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Form;
